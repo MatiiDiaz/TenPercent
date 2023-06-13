e9611500c340b960b6db842b9a5c4f6f0d65081a
MatiiDiaz <matt03.06.01@gmail.com>
2023-06-13 09:59:09 -0400

    Version 1.1

--- app/src/main/java/com/example/tenpercent/MainActivity.java
+++ app/src/main/java/com/example/tenpercent/MainActivity.java
@@ -10,6 +10,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
@@ -17,56 +18,76 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
-    MediaPlayer musicaVictoria, musicaFondo;
-    private TextView tv2;
-    public Button button, button2;
+    private MediaPlayer musicaVictoria, musicaFondo;
+    private TextView tv2, tvPuntos;
+    private Button button, button2;
     private GestureDetector gestos;
     public ImageButton imageButton, imageButton2, imageButton3;
     private SensorManager sensorManager;
     private Sensor accelerometer;
     private SensorEventListener evento;
-    private boolean n = false;
-    private int contador = 0;
+    private boolean juegoIniciado = false;
+    private int puntos = 0;
+
+    private final int[] accionIdArray = {
+            R.string.simple_tap,
+            R.string.doble_tap,
+            R.string.presion_larga,
+            R.string.agitacion,
+            R.string.deslizar_arriba,
+            R.string.deslizar_abajo
+    };
+    private int accionActualId;
+    private Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         musicaFondo = MediaPlayer.create(this, R.raw.giovanni_giorgio);
         musicaVictoria = MediaPlayer.create(this, R.raw.siuu);
-        button=findViewById(R.id.button);
-        button2=findViewById(R.id.button2);
-        tv2=findViewById(R.id.tv2);
+
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        tv2 = findViewById(R.id.tv2);
+        tvPuntos = findViewById(R.id.tvPuntos);
         gestos = new GestureDetector(this, new ListenerGestos());
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        random = new Random();
+
         button.setEnabled(true);
         button2.setEnabled(false);
-        musicaFondo.start();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarJuego();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                detenerJuego();
+            }
+        });
+
         evento = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
-                if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                    Resources res = getResources();
-                    String acceleration = res.getString(R.string.agitacion);
-                    if ((event.values[0]>8 || event.values[0]<-8) && n && contador==0){
-                        contador++;
-                    } else {
-                        if ((event.values[0]>-8 || event.values[0]<8) && n && contador==1){
-                            musicaFondo.pause();
-                            musicaVictoria.start();
-                            musicaVictoria.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                                @Override
-                                public void onCompletion(MediaPlayer mp) {
-                                    // Restablece el estado del MediaPlayer y vuelve a inicializarlo
-                                    musicaFondo.start();
-                                }
-                            });
-                            contador=0;
-                            tv2.setText(R.string.agitacion);
-                        }
+                if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER && juegoIniciado) {
+                    if ((event.values[0] > 8 || event.values[0] < -8) && accionActualId == R.string.agitacion) {
+                        musicaVictoria.start();
+
+                        incrementarPuntos();
+                        actualizarAccionActual();
                     }
                 }
             }
@@ -76,35 +97,79 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
-        sensorManager.registerListener(evento, accelerometer, SensorManager.SENSOR_DELAY_UI);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                button.setEnabled(false);
-                button2.setEnabled(true);
-                n=true;
-                if (!musicaFondo.isPlaying()) {
-                    // Restablece el estado del MediaPlayer y vuelve a inicializarlo
-                    musicaFondo.reset();
-                    musicaFondo = MediaPlayer.create(MainActivity.this, R.raw.giovanni_giorgio);
-                    musicaFondo.start();
-                }
+    }
+
+    private void iniciarJuego() {
+        if (!juegoIniciado) {
+            juegoIniciado = true;
+            button.setEnabled(false);
+            button2.setEnabled(true);
+            tvPuntos.setText(String.valueOf(puntos));
+
+            if (!musicaFondo.isPlaying()) {
+                musicaFondo.start();
             }
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                button.setEnabled(true);
-                button2.setEnabled(false);
-                n=false;
-                if (musicaFondo.isPlaying()) {
-                    musicaFondo.pause();
-                }
-                // Restablece el estado del MediaPlayer y vuelve a inicializarlo
-                musicaFondo.reset();
-                musicaFondo = MediaPlayer.create(MainActivity.this, R.raw.giovanni_giorgio);
-            }
-        });
+
+            iniciarNuevaAccion();
+        }
+    }
+
+    private void detenerJuego() {
+        if (juegoIniciado) {
+            juegoIniciado = false;
+            button.setEnabled(true);
+            button2.setEnabled(false);
+
+            detenerMusica();
+
+            mostrarPuntos();
+            reiniciarJuego();
+        }
+    }
+
+    private void iniciarNuevaAccion() {
+        accionActualId = obtenerAccionAleatoria();
+        tv2.setText(accionActualId);
+    }
+
+    private int obtenerAccionAleatoria() {
+        int randomIndex = random.nextInt(accionIdArray.length);
+        return accionIdArray[randomIndex];
+    }
+
+    private void actualizarAccionActual() {
+        if (juegoIniciado) {
+            accionActualId = obtenerAccionAleatoria();
+            tv2.setText(accionActualId);
+        }
+    }
+
+    private void incrementarPuntos() {
+        puntos++;
+        tvPuntos.setText(String.valueOf(puntos));
+    }
+
+    private void detenerMusica() {
+        if (musicaFondo.isPlaying()) {
+            musicaFondo.pause();
+        }
+        if (musicaVictoria.isPlaying()) {
+            musicaVictoria.pause();
+        }
+    }
+
+    private void mostrarPuntos() {
+        // Detenemos la música si está reproduciéndose
+        detenerMusica();
+
+        // Mostramos los puntos obtenidos
+        String mensajePuntos = getResources().getString(puntos);
+        tv2.setText(mensajePuntos);
+    }
+
+    private void reiniciarJuego() {
+        puntos = 0;
+        tvPuntos.setText(String.valueOf(puntos));
     }
 
     @Override
@@ -113,71 +178,81 @@ public class MainActivity extends AppCompatActivity {
         return super.onTouchEvent(event);
     }
 
-    class ListenerGestos extends GestureDetector.SimpleOnGestureListener{
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(evento);
+        detenerMusica();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(evento, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        if (juegoIniciado && !musicaFondo.isPlaying()) {
+            musicaFondo.start();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (musicaFondo != null) {
+            musicaFondo.release();
+        }
+        if (musicaVictoria != null) {
+            musicaVictoria.release();
+        }
+    }
+
+    class ListenerGestos extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onSingleTapUp(@NonNull MotionEvent e) {
+            if (juegoIniciado && accionActualId == R.string.simple_tap) {
+                musicaVictoria.start();
+
+                incrementarPuntos();
+                actualizarAccionActual();
+            }
+            return true;
+        }
+
         @Override
         public void onLongPress(@NonNull MotionEvent e) {
-            if(n){
-                musicaFondo.pause();
+            if (juegoIniciado && accionActualId == R.string.presion_larga) {
                 musicaVictoria.start();
-                musicaVictoria.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mp) {
-                        // Restablece el estado del MediaPlayer y vuelve a inicializarlo
-                        musicaFondo.start();
-                    }
-                });
-                tv2.setText(R.string.presion_larga);
+
+                incrementarPuntos();
+                actualizarAccionActual();
             }
         }
 
         @Override
         public boolean onDoubleTap(@NonNull MotionEvent e) {
-            if(n){
-                musicaFondo.pause();
+            if (juegoIniciado && accionActualId == R.string.doble_tap) {
                 musicaVictoria.start();
-                musicaVictoria.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mp) {
-                        // Restablece el estado del MediaPlayer y vuelve a inicializarlo
-                        musicaFondo.start();
-                    }
-                });
-                tv2.setText(R.string.doble_tap);
+
+                incrementarPuntos();
+                actualizarAccionActual();
             }
             return true;
         }
 
         @Override
         public boolean onFling(@NonNull MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
-            if(n){
-                if(e2.getX()>e1.getX()){
-                    if(e2.getY()>e1.getY()) {
-                        musicaFondo.pause();
-                        musicaVictoria.start();
-                        // Restablece el estado del MediaPlayer y vuelve a inicializarlo
-                        musicaFondo.start();
-                        tv2.setText(R.string.der_abajo);
-                    } else {
-                        musicaFondo.pause();
-                        musicaVictoria.start();
-                        // Restablece el estado del MediaPlayer y vuelve a inicializarlo
-                        musicaFondo.start();
-                        tv2.setText(R.string.der_arriba);
-                    }
-                } else {
-                    if(e2.getY()>e1.getY()) {
-                        musicaFondo.pause();
-                        musicaVictoria.start();
-                        // Restablece el estado del MediaPlayer y vuelve a inicializarlo
-                        musicaFondo.start();
-                        tv2.setText(R.string.izq_abajo);
-                    } else {
-                        musicaFondo.pause();
-                        musicaVictoria.start();
-                        // Restablece el estado del MediaPlayer y vuelve a inicializarlo
-                        musicaFondo.start();
-                        tv2.setText(R.string.izq_arriba);
-                    }
+            if (juegoIniciado) {
+                boolean acierto = false;
+                if (e2.getY() > e1.getY() && accionActualId == R.string.deslizar_abajo) {
+                    acierto = true;
+                } else if (e2.getY() < e1.getY() && accionActualId == R.string.deslizar_arriba) {
+                    acierto = true;
+                }
+
+                if (acierto) {
+                    musicaVictoria.start();
+                    incrementarPuntos();
+                    actualizarAccionActual();
                 }
             }
             return true;
@@ -185,9 +260,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void music(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.imageButton:
-                if (musicaVictoria == null){
+                if (musicaVictoria == null) {
                     musicaVictoria = MediaPlayer.create(this, R.raw.siuu);
                 }
                 musicaVictoria.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
@@ -199,12 +274,12 @@ public class MainActivity extends AppCompatActivity {
                 musicaVictoria.start();
                 break;
             case R.id.imageButton2:
-                if(musicaVictoria != null){
+                if (musicaVictoria != null) {
                     musicaFondo.pause();
                 }
                 break;
             case R.id.imageButton3:
-                if (musicaVictoria == null){
+                if (musicaVictoria == null) {
                     musicaVictoria = MediaPlayer.create(this, R.raw.siuu);
                 }
                 musicaVictoria.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
@@ -219,38 +294,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void stopMusic() {
-        musicaVictoria.release();
-        musicaVictoria = null;
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener(evento);
-        if (musicaFondo.isPlaying()) {
-            musicaFondo.pause();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener(evento, accelerometer, SensorManager.SENSOR_DELAY_UI);
-        if (n && !musicaFondo.isPlaying()) {
-            // Restablece el estado del MediaPlayer y vuelve a inicializarlo
-            musicaFondo.reset();
-            musicaFondo = MediaPlayer.create(MainActivity.this, R.raw.giovanni_giorgio);
-            musicaFondo.start();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (musicaFondo != null) {
-            musicaFondo.release();
-        }
         if (musicaVictoria != null) {
             musicaVictoria.release();
+            musicaVictoria = null;
         }
     }
 }
